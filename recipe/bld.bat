220701aef1a48e0f569f8a09cbd810b121e160f7
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-14 16:05:56 +0200

    Initial feedstock commit with conda-smithy 3.7.4.dev54.

---
+++ recipe/bld.bat
@@ -0,0 +1,17 @@
+mkdir build
+if errorlevel 1 exit 1
+cd build
+if errorlevel 1 exit 1
+cmake ^
+    -G "MSYS Makefiles" ^
+    -DCMAKE_CXX_STANDARD=17 ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCSPICE_BUILD_STATIC_LIBRARY=1 ^
+    ..
+if errorlevel 1 exit 1
+cmake --build . --config RelWithDebInfo --target install
+if errorlevel 1 exit 1
+ctest
+if errorlevel 1 exit 1
